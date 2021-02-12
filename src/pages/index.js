919950bb750dab5f912a70435e901ac8ea51dcb2
Sotono <sotono.dk@gmail.com>
2021-02-12 15:44:19 +0900

    typoと記事追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Bio from "../components/bio"
-import Cards from "../components/Cards"
+import Cards from "../components/cards"
 
 const BlogIndex = ({ data }) => {
   return (
