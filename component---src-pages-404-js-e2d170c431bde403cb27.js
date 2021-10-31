601b769d3f0ee60396dd540c6cbb301063665f71
Sotono <sotono.dk@gmail.com>
2021-10-31 23:15:01 +0900

    Updates

--- component---src-pages-404-js-e2d170c431bde403cb27.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var l=a(7294),n=a(7198),r=a(6179);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return l.createElement(n.Z,{location:a,title:o},l.createElement(r.Z,{title:"404: Not Found"}),l.createElement("h1",null,"404: Not Found"))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e2d170c431bde403cb27.js.map
